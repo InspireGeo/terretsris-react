1efc25b29289e63841c19ebe9c88fb876134fcc0
InspireGeo <aytacaraz@gmail.com>
2022-01-12 09:13:38 +0100

    ilk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+/* import 'antd/dist/antd.css';
+import './react-geo.css'; */
+
+import {
+  SimpleButton
+} from '@terrestris/react-geo';
 
 function App() {
   return (
@@ -9,14 +16,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SimpleButton
+          onClick={() => {alert('Hello World!');}}
+          iconName="bars"
+        />
       </header>
     </div>
   );
